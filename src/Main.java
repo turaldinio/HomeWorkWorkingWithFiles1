f6b6c4937474a1cb68ccfcc80a0fcfda46cb2a7d
Tural Guluev <tural-98@bk.ru>
2022-06-06 15:34:04 +0300

    create 2 new function, for create files/directory and writing information to  file

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,70 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean successful = true;
 
-        boolean check = true;
+        System.out.println("Создание директории/файла \n" +
+                "Укажите полный путь до директории/файла \n" +
+                "Для выхода нажмите enter ");
 
-        File game = new File("C:\\Game");
-        check = check && game.mkdir();
+        Scanner scan = new Scanner(System.in);
 
-        File src = new File(game.getPath() + "\\src");
-        check = check && src.mkdir();
+        String request = scan.nextLine();
 
-        File res = new File(game.getPath() + "\\res");
-        check = check && res.mkdir();
-
-        File savegames = new File(game.getPath() + "\\savegames");
-        check = check && savegames.mkdir();
-
-        File temp = new File(game.getPath() + "\\temp");
-        check = check && temp.mkdir();
-
-        File main = new File(src.getPath() + "\\main");
-        check = check && main.mkdir();
-
-        File test = new File(src.getPath() + "\\test");
-        check = check && test.mkdir();
-
-        File main_java = new File(main.getPath() + "\\Main.java");
-        try {
-            check = check && main_java.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File utils_java = new File(main.getPath() + "\\Utils.java");
-        try {
-            check = check && utils_java.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File drawable = new File(res.getPath() + "\\drawable");
-        check = check && drawable.mkdir();
-
-
-        File vectors = new File(res.getPath() + "\\vectors");
-        check = check && vectors.mkdir();
-
-        File icon = new File(res.getPath() + "\\icon");
-        check = check && icon.mkdir();
-
-        File temp_txt = new File(temp.getPath() + "\\temp.txt");
-        try {
-            check = check && temp_txt.createNewFile();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (FileWriter writer = new FileWriter(temp_txt)) {
-            if (check) {
-                writer.write("Все папки успешно созданы");
-            } else {
-                writer.write("Ошибка создания всех папок");
+        while (!request.isEmpty()) {
+            try {
+                successful &= createFilesOrDirectory(request);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            System.out.println("\nУкажите полный путь до директории/файла ");
+            request = scan.nextLine();
+        }
 
-            writer.flush();
+        createFinalFile(successful);
+    }
+
+
+    private static boolean createFilesOrDirectory(String path) throws IOException {
+        File file = new File(path);
+
+        if (!path.contains(".")) {
+            if (file.mkdir()) {
+                System.out.printf("Директория %s успешна создана", path);
+                return true;
+            } else {
+                System.out.printf("Не удалось создать директорию %s ", path);
+                return false;
+            }
+        } else {
+            if (file.createNewFile()) {
+
+                System.out.printf("Файл %s успешно создан", path);
+                return true;
+            } else {
+                System.out.printf("Не удалось создать файл %s ", path);
+                return false;
+            }
+        }
+    }
+
+    private static void createFinalFile(boolean successful) {
+        String file = String.format("C:%sGame%stemp%stemp.txt", File.separator, File.separator, File.separator);
+
+        try (FileWriter writer = new FileWriter(file)) {
+            if (successful) {
+                writer.write("Все файлы успешно созданы");
+            } else {
+                writer.write("Не удалось создать все файлы");
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
 }
